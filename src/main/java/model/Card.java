7a83e49e1d48e05b6410ad9a606b4035ef6131ed
XYA <xueyeai@gmail.com>
2014-02-15 05:36:25 -0800

    init Project

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -4,4 +4,5 @@ package model;
  * Created by xya on 2/15/14.
  */
 public class Card {
+
 }
--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -4,4 +4,17 @@ package model;
  * Created by xya on 2/15/14.
  */
 public class Player {
+    public void hit() {
+        System.out.println("hit");
+    }
+
+    public void stand() {
+        System.out.println("stand");
+
+    }
+
+    public void action() {
+        System.out.println("action");
+
+    }
 }
