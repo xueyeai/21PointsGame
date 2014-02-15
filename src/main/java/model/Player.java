f08552a4480fa03bcc47d4d6606201eb7ea1a916
XYA <xueyeai@gmail.com>
2014-02-15 05:33:37 -0800

    init Project

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by xya on 2/15/14.
+ */
+public class Player {
+}
---
+++ src/test/java/model/CardTest.java
@@ -0,0 +1,9 @@
+package model;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by xya on 2/15/14.
+ */
+public class CardTest extends TestCase {
+}
